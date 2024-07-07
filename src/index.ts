c877b565324e21a57d94bc06d664e1c44426411f
Dmitry Gurovich <yrtimid@gmail.com>
2024-07-07 18:01:38 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export async function main(): Promise<void> {
 	let aggregateNotificationsLimit = Number.parseInt(core.getInput('aggregate_notifications_limit', { required: false }), 10) || 5;
 	let options: Options = { shaFrom, shaTo, codewatchers, codewatchersRef, ignoreOwn, aggregateFilesLimit, aggregateNotificationsLimit };
 	core.debug(JSON.stringify({ options }, null, 2));
-		
+
 	let notifications = await check(context, options);
 	notifications = aggregateCommits(context, options, notifications);
 	notifications = aggregateFiles(context, options, notifications);
